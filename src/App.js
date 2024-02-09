c406b1528183f1e0e75309d7d03d0b3b6879e017
vikas bisariya <vikasbisariya20@gmail.com>
2024-02-09 10:41:27 +0530

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { EmployeeData } from "./EmployeeData";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [id, setId] = useState(0);
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("are you sure to delete this item?")) {
+        const dt = data.filter((item) => item.id !== id);
+        setData(dt);
+      }
+    }
+  };
+
+  const handleEdit = (id) => {
+     const dt = data.filter((item)=> item.id === id);
+     if(dt!==undefined){
+        setId(id);
+        setFirstname(dt[0].firstName);
+        setLastname(dt[0].lastName);
+        setAge(dt[0].age);
+     }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div style={{display: 'flex', justifyContent:'center', gap:'20px',marginTop:'10px', marginBottom:'50px'}} >
+        <div>
+          <label>
+            First Name:-
+            <input
+              type="text"
+              placeholder="Enter first name"
+              onChange={(e) => setFirstname(e.target.value)}
+              value={firstName}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Last Name:-
+            <input
+              type="text"
+              placeholder="Enter last name"
+              onChange={(e) => setLastname(e.target.value)}
+              value={lastName}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Age:-
+            <input
+              type="number"
+              placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
+            />
+          </label>
+        </div>
+      </div>
+
+      <table className="table table-hover">
+        <thead>
+          <th>Sr.No.</th>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Second Name</th>
+          <th>Age</th>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.id}</td>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.age}</td>
+                <td>
+                  <button
+                    className="btn btn-primary m-2"
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/EmployeeData.js
@@ -0,0 +1,53 @@
+
+ export const EmployeeData = [
+    {
+        id: 1,
+        firstName : "vikas",
+        lastName : "bisariya",
+        age: 22
+    },
+    {
+        id: 2,
+        firstName : "ashish",
+        lastName : "xyz",
+        age: 23
+    },
+    {
+        id: 3,
+        firstName : "avinash",
+        lastName : "xyz",
+        age: 24
+    },
+    {
+        id: 4,
+        firstName : "vishal",
+        lastName : "xyz",
+        age: 24
+    },
+    {
+        id: 5,
+        firstName : "riya",
+        lastName : "xyzasdf",
+        age: 24
+    },
+    {
+        id: 5,
+        firstName : "veeru",
+        lastName : "xyzsfd",
+        age: 25
+    },
+    {
+        id: 5,
+        firstName : "sumit",
+        lastName : "xysfdz",
+        age: 21
+    },
+    {
+        id: 5,
+        firstName : "roy",
+        lastName : "xyz",
+        age: 20
+    }
+]
+
+// export default EmployeeData;
